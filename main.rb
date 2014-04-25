cb5dbf6377ce9961763cd48f0df191a583cc293b
asilbalaban <mail@asilbalaban.com>
2014-04-25 22:31:33 +0300

    Tekrarlayan kayıt engelleme
    Eğer son kaydedilen değer şimdiki alexa değeri ile aynı ise o zaman
    dosyaya kaydetmeme özelliği eklendi

--- main.rb
+++ main.rb
@@ -8,12 +8,24 @@ xml_data = Net::HTTP.get_response(URI.parse('http://data.alexa.com/data?cli=10&d
 doc = REXML::Document.new(xml_data)
 
 doc.elements.each('ALEXA/SD/POPULARITY') do |ele|
-	$alexaValue =  ele.attributes["TEXT"]
+	$alexaValue =  ele.attributes["TEXT"].to_s
 	break
 end
 
-$today 	= Date.today
-$echo 	= "#{$today} - #{$alexaValue} - #{url} \n"
+# Son değeri kontrol et
+lastLine = File.open("alexa.txt") { |f| f.extend(Enumerable).inject { |_,ln| ln } }
+lastRank = lastLine.match(/- (.*?) -/).captures[0]
+
+# Şimdiki ile aynıysa o zaman dosyaya ekleme yapma
+if lastRank == $alexaValue
+	puts "Deger ayni, yazma yok"
+else
+	# Eğer değer değiştiyse dosyaya ekle
+	puts "Yeni deger kaydediliyor"
+
+	$today 	= Date.today
+	$echo 	= "#{$today} - #{$alexaValue} - #{url} \n"
+	File.open("alexa.txt", 'a+') {|f| f.write($echo) }
+end
 
 
-File.open("alexa.txt", 'a+') {|f| f.write($echo) }
